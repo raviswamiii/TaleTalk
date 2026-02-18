cc1ed8c6581258988b73b68804eb8eeea97d2371
raviswamiii <raviswamisamsung@gmail.com>
2026-02-18 18:35:56 +0530

    connectedToDatabase

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,15 @@
-import express from 'express'; 
+import "dotenv/config";
+import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
+import databaseConnection from "./config/mongodb.js";
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+databaseConnection();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
