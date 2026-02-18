7ece9cf0086ead5fdfa10418945ba1b8a82e7d29
raviswamiii <raviswamisamsung@gmail.com>
2026-02-18 23:34:24 +0530

    addedBlurScreen

--- Frontend/src/games/GameOne.jsx
+++ Frontend/src/games/GameOne.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { RiAddLine } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import { RxCross1 } from "react-icons/rx";
 
 export const GameOne = () => {
+  const [blurScreen, setBlurScreen] = useState(false);
   const newQuestions = [
     { question: "What is the capital of France?" },
     { question: "Who wrote 'Romeo and Juliet'?" },
@@ -22,16 +23,24 @@ export const GameOne = () => {
     setCurrentIndex((prev) => prev + 1);
   };
 
+  const addNewQuestion = () => {};
   return (
-    <div className="relative h-screen bg-[#0B090A] flex flex-col">
+    <div className="relative h-screen bg-[#0B090A] flex flex-col z-10">
       <div className="bg-[#161214] text-white px-3 py-3 flex items-center justify-between">
         <div className="flex gap-4 items-center">
           <HiMenuAlt2 className="text-2xl" />
           <h1 className="font-semibold text-lg">Question Generator</h1>
         </div>
-        <Link to="/addQuestions" className="text-white">
-          <RiAddLine className="text-2xl" />
-        </Link>
+        <RiAddLine onClick={() => setBlurScreen(true)} className="text-2xl" />
+      </div>
+
+      <div
+        className={`bg-black/40 h-screen absolute inset-0 z-20 backdrop-blur-sm ${blurScreen ? "" : "hidden"}`}
+      >
+        <RxCross1
+          onClick={() => setBlurScreen(false)}
+          className="text-white text-lg absolute right-4 top-3"
+        />
       </div>
 
       <div className="h-screen p-6 overflow-y-scroll">
@@ -50,6 +59,8 @@ export const GameOne = () => {
               );
             })
           : ""}
+
+        {/* <div className="bg-green-500 h-[60vh] rounded-xl"></div> */}
       </div>
 
       <div className="absolute bottom-0 w-full flex flex-col items-center">
