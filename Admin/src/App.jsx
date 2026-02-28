dffccc107baa6305640d60138134fad473f362c4
raviswamiii <raviswamisamsung@gmail.com>
2026-02-28 14:34:17 +0530

    adminSignInFrontendPart

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { QuestionContainer } from './components/QuestionContainer'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { QuestionContainer } from "./components/QuestionContainer";
+import { Route, Routes } from "react-router-dom";
+import { SignIn } from "./pages/SignIn";
 
 export const App = () => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : "",
+  );
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  });
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<QuestionContainer />} />
-      </Routes>
+      {token === "" ? (
+        <SignIn setToken={setToken} />
+      ) : (
+        <Routes>
+          <Route path="/" element={<QuestionContainer />} />
+        </Routes>
+      )}
     </div>
-  )
-}
+  );
+};
